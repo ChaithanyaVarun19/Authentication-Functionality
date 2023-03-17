697b6c1b3abce22ca1e1c5969422516670add7bb
ChaithanyaVarun19 <chaitanyavarun19@gmail.com>
2023-03-17 17:38:47 +0000

    Authentication Functionality

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import {Link, withRouter} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <ul className="nav-container">
+    <li className="nav-item">
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/about">
+        About
+      </Link>
+    </li>
+  </ul>
+)
+
+export default withRouter(Header)
